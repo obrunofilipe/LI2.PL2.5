d5604b063d73e183ec2dea3c7e7c173a50f04a90
Bruno Filipe <brunofilipe377@gmail.com>
2020-03-12 13:54:12 +0000

    incilializaçao do tabuleiro e do estado

--- camada_de_dados.c
+++ camada_de_dados.c
@@ -1,15 +1,37 @@
 #include <stdio.h>
 #include "camada_de_dados.h"
 
-
-ESTADO *inicializar_estado(){
-	
+void inicializar_tabuleiro(CASA *tab){
+    int linha,coluna;
+    for(linha = 0; linha < 8 ; linha++){
+        if(linha == 3){
+            for(coluna = 0 ; coluna < 8 ; coluna++){
+                if(coluna == 4)
+                    tab [linha] [coluna] = BRANCA;
+                else
+                    tab [linha] [coluna] = VAZIO;
+            }
+        }
+        else {
+            for(coluna = 0; coluna < 8 ; coluna++)
+                tabuleiro [linha] [coluna] = VAZIO;
+        }
+    }
 }
 
-/*
-	CASA tab[8][8];             //armazena informação sobre o tabuleiro
-	COORDENADA ultima_jogada;	//a coordenada da última jogada
-	JOGADAS jogadas;			//armazena informação sobre as jogadas
-	int num_jogadas;			//indica quantas jogadas foram efetuadas
-	int jogador_atual;
-*/
+
+
+
+ESTADO *inicializar_estado(){
+    CASA tabuleiro[8][8]; // declarar o tabuleiro
+    ESTADO estadoJogo;    // declarar o estado
+    inicializar_tabuleiro(tabuleiro);  // modifica a matriz  tabuleiro
+	estadoJogo.jogador_atual = 1;      // inicializa o jogador_atual
+	estadoJogo.num_jogadas = 0;        // inicializa o número de jogadas
+	strcpy(estadoJogo.tab, tabuleiro); // copia a matris modificada para a matriz do estado
+	estadoJogo.ultima_jogada = {5,4};
+
+
+
+}
+
