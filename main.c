9e41fd18ac0ad4020fb8253c573005b1bbe9a5d2
João <jpdelgado2001@gmail.com>
2020-03-13 20:25:36 +0000

    loop

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 #include "camada_de_dados.h"
 #include "camada_de_interface.h"
-
+#include <string.h>
 int main()
 {
     ESTADO *e = inicializar_estado();
-    interpretador(e);
+    while (e->num_jogadas < 32){
+        interpretador(e);
+    }
     return 0;
 }
-
