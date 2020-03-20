3483bfb5cb7827f60f60c3b33d3e436cc9c023a8
Alex <a93202@alunos.uminho.pt>
2020-03-20 12:14:47 +0000

    Documentada a camada de dados

--- camada_de_dados.h
+++ camada_de_dados.h
@@ -2,51 +2,127 @@
 #define ___CAMADA_DE_DADOS_H___
 
 /**
-@file camada_de_dados.h
-Definição do estado e das funções que o manipulam
+ @file dados,h
+ Definições dos dados presentes no estado e das funções que o alteram
 */
 
 //Aqui dentro aparece o código correspondente aos protótipos
-//CASA
 /**
-\brief Tipo de dados para a casa
-*/
+\brief Tipos de dados que uma casa pode assumir
+ Uma casa na qual esteja um jogador é BRANCA, na qual já tenha passsado um jogador é PRETA, se não reunir nenhuma das condições anteriores é VAZIA.
+ */
 typedef enum {VAZIO, BRANCA, PRETA} CASA;
 
-//COORDENADA
+/**
+\brief Tipo de dados para definir uma coordenada
+ */
 typedef struct {
-	int coluna;
-	int linha;
+    int coluna;
+    int linha;
 } COORDENADA;
 
+/**
+\brief Tipo de dados para definir uma jogada
+ */
 //JOGADA
 typedef struct {
-	COORDENADA jogador1;
-	COORDENADA jogador2;
+    COORDENADA jogador1;
+    COORDENADA jogador2;
 } JOGADA;
 
+/**
+\brief Tipo de dados para guardar todas as jogadas efetuadas por um determinado jogador, o número máximo de jogadas possíveis são 32
+ */
 //JOGADAS
 typedef JOGADA JOGADAS[32];
 
+/**
+\brief Tipo de dados que definem o estado
+ */
 //ESTADO
 typedef struct {
-	CASA tab[8][8];             //armazena informação sobre o tabuleiro
-	COORDENADA ultima_jogada;	//a coordenada da última jogada
-	JOGADAS jogadas;			//armazena informação sobre as jogadas
-	int num_jogadas;			//indica quantas jogadas foram efetuadas
-	int jogador_atual;			//indica qual é o jogador a jogar
-	int num_comandos;           //aramzena o número total de comandos
+    /** O tabuleiro */
+    CASA tab[8][8];             //armazena informação sobre o tabuleiro
+    /** A coordenada da última jogada */
+    COORDENADA ultima_jogada;	//a coordenada da última jogada
+    /** Armazena a informação sobre as jogadas */
+    JOGADAS jogadas;			//armazena informação sobre as jogadas
+    /** Número de jogadas, utilizado no prompt */
+    int num_jogadas;			//indica quantas jogadas foram efetuadas
+    /**Número do jogador atual */
+    int jogador_atual;			//indica qual é o jogador a jogar
+    /** Número do comando atual, utilizado no prompt */
+    int num_comandos;           //aramzena o número total de comandos
 } ESTADO;
 
 
-ESTADO *inicializar_estado();                    //Esta função deverá criar um estado vazio (com o tabuleiro inicializado)
-void inicializar_tabuleiro(CASA tab[8][8]);      //Função que inicializa o tabuleiro
-int obter_jogador_atual(ESTADO *estado);		 //Esta função permite obter o número do jogador atual
-int obter_numero_de_jogadas(ESTADO *estado);	 //Esta função permite obter quantas jogadas foram efetuadas (cada jogada tem o movimento de dois jogadores)
-CASA obter_estado_casa(ESTADO *e, COORDENADA c); //Esta função permite obter o estado atual da casa
-void atualiza_estado_jogo (ESTADO *e, COORDENADA c);//Esta função atualiza o estado do jogo apos a jogada
-void incrementa_num_comandos(ESTADO *e);            // Funcao que altera o numero de comandos na camada de dados
-int obter_num_comandos(ESTADO *e);                  // Retorna o número atual de comandos;
+
+/**
+\brief Função para criar o estado vazio
+@returns O novo estado
+*/
+ESTADO *inicializar_estado();
+
+/**
+\brief Função para inicializar o tabuleiro
+@param tab Tabuleiro
+@returns O Tabuleiro inicial
+*/
+void inicializar_tabuleiro(CASA tab[8][8]);
+
+/**
+\brief Função para obter o número do jogador atual
+@param estado Apontador para o estado atual
+@return O número do jogador atual
+*/
+int obter_jogador_atual(ESTADO *estado);
+
+/**
+\brief Função para obter o número de jogadas efetuadas (cada jogada tem o movimento de dois jogadores)
+@param estado Apontador para o estado atual
+@return O número de jogadas efetuadas
+*/
+int obter_numero_de_jogadas(ESTADO *estado);
+
+/**
+\brief Função para obter o estado de uma casa do tabuleiro
+@param e Apontador para o estado atual
+@param c A coordenada
+@return Se a casa está VAZIA, BRANCA ou PRETA
+ */
+CASA obter_estado_casa(ESTADO *e, COORDENADA c);
+
+/**
+\brief Função que atualiza o estado de jogo após cada jogada
+@param e Apontador para o estado atual
+@param c A coordenada
+*/
+void atualiza_estado_jogo (ESTADO *e, COORDENADA c);
+
+/**
+\brief Função que incrementa o número de comandos
+@param e Apontador para o estado atual
+*/
+void incrementa_num_comandos(ESTADO *e);
+
+/**
+\brief Função para obter o número atual de comandos, utilizado no prompt
+@param e Apontador para o estado atual
+@return O número de comandos atual
+ */
+int obter_num_comandos(ESTADO *e);
+
+/**
+\brief Função para alterar o valor do número de jogadas, para quebrar o loop no main e para terminar o programa
+ * @param e Apontador para o estado atual
+ * @param numJogadas O número de jogads atuais
+ */
 void modifica_num_jogadas (ESTADO *e , int numJogadas); // ultilizada para alterar o valor do numero de jogadas atual para quebrar o loop no main e terminar o programa
-COORDENADA obter_ultima_jogada(ESTADO *e );             // retorna a ultima jogada
+
+/**
+\brief Função para obter a última jogada efetuada
+ * @param e Apontador para o estado atual
+ * @return A última jogada efetuada
+ */
+COORDENADA obter_ultima_jogada(ESTADO *e );
 #endif
