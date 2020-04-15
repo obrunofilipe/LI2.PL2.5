5466c189e4510a02ed6b9578bd69bad4c0f1e33c
Bruno Filipe <brunofilipe377@gmail.com>
2020-04-15 18:06:49 +0100

    criaçao do modulo de listas e implementaçao das funçoes de listas

---
+++ listas.c
@@ -0,0 +1,40 @@
+#include <_stdio.h>
+#include <stdlib.h>
+#include "listas.h"
+
+LISTA criar_lista(){
+    LISTA  l;
+    l = malloc( sizeof(NODO) );
+    return l;
+}
+
+LISTA insere_cabeca(LISTA L, void *valor){
+    LISTA  nodo_novo;
+    nodo_novo = criar_lista();
+    nodo_novo->valor   = valor;
+    nodo_novo->proximo = L;
+
+}
+
+void *devolve_cabeca(LISTA L){
+    return  L->valor;
+}
+
+LISTA proximo(LISTA L){
+    L = L->proximo;
+    return L;
+}
+
+LISTA remove_cabeca(LISTA L){
+    LISTA C;
+    C = L;
+    L = L->proximo;
+    free(C);
+    return L;
+}
+int lista_esta_vazia(LISTA L){
+    if(L == NULL)
+        return 1;
+    else
+        return 0;
+}
---
+++ listas.h
@@ -0,0 +1,19 @@
+#ifndef _LISTAS_H
+#define _LISTAS_H
+typedef struct nodo {
+    void *valor;
+    struct nodo *proximo;
+} NODO, *LISTA;
+//cria uma lista
+LISTA criar_lista();
+//insere um novo elemento à cabeça
+LISTA insere_cabeca(LISTA L, void *valor);
+//devolve a cabeça da Lista
+void *devolve_cabeca(LISTA L);
+//devolve a cauda da lista
+LISTA proximo(LISTA L);
+// remove a cabeça da lista libertando o espaço ocupado e retorna a cauda
+LISTA remove_cabeca(LISTA L);
+//Devolve verdadeiro se a lista é vazia
+int lista_esta_vazia(LISTA L);
+#endif
